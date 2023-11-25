cd1b8645a5b1fc34be0a48df04a5ccfd8f0cc00f
anu16 <albert.ugwudike16@imperial.ac.uk>
2023-11-25 13:07:35 +0000

    renaming changes

--- StarMap/train.py
+++ StarMap/train.py
@@ -20,11 +20,11 @@ def train():
 
     device = torch.device('cuda' if cuda_available else 'cpu')
 
-    flatmap = StarMap().to(device)
+    starmap = StarMap().to(device)
 
     criterion = nn.MSELoss().to(device)
 
-    optimizer = torch.optim.Adam(flatmap.parameters(), lr=0.01)
+    optimizer = torch.optim.Adam(starmap.parameters(), lr=0.01)
     scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.5)
 
     num_epochs = 1000
@@ -36,12 +36,12 @@ def train():
 
     for epoch in range(num_epochs):
             
-        flatmap.train()
+        starmap.train()
 
         for vol in data_loader:
             vol = vol.to(device)
             s_img = torch.cat((vol.real, vol.imag), 1).to(device)
-            param_map = flatmap(s_img)
+            param_map = starmap(s_img)
             B0_offset_map, init_phase_map = estimate_delta_omega(vol)
             sim_img = simulate_volume(param_map, B0_offset_map, init_phase_map, device=device)
 
@@ -57,13 +57,13 @@ def train():
         # if train_losses[-1] < 0.15 and scheduler.get_lr() == 0.05: 
         #     scheduler.step()
 
-        flatmap.eval()
+        starmap.eval()
 
         with torch.no_grad():
             _, vol =  next(enumerate(DataLoader(test_dataset, batch_size=7)))
             vol = vol.to(device)
             s_img = torch.cat((vol.real, vol.imag), 1).to(device)
-            param_map = flatmap(s_img)
+            param_map = starmap(s_img)
             B0_offset_map, init_phase_map = estimate_delta_omega(vol)
             sim_img = simulate_volume(param_map, B0_offset_map, init_phase_map, device=device)
 
@@ -73,13 +73,13 @@ def train():
             validation_losses.append(loss.data.item())
         
 
-        save_model(flatmap, train_losses, validation_losses, "./trained_models/starmap1.pt")
+        save_model(starmap, train_losses, validation_losses, "./trained_models/starmap1.pt")
 
         print(f"Epoch: {epoch}, Train Loss: {train_losses[-1]}, Validation Loss: {validation_losses[-1]}")
 
 
-def save_model(flatmap, train_losses, validation_losses, filename):
-    torch.save(flatmap.state_dict(), filename)
+def save_model(starmap, train_losses, validation_losses, filename):
+    torch.save(starmap.state_dict(), filename)
     losses = torch.tensor([train_losses, validation_losses]).T
     pd.DataFrame(data = losses, columns=["Train Loss", "Validation Loss"]).to_csv("./losses/starmap1_loss.csv")
     print(f"Saved current model parameters in {filename}")
